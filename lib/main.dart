57aa78f278d42e060f4b874e8149cdddc71799ca
shalika <shalikalahiru@gmail.com>
2020-01-18 22:09:23 +0530

    creat dice screen

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+         Expanded(
+           child: Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Image(image: AssetImage('images/dice1.png'),
+             ),
+           ),
+         ),
+         Expanded(
+           child: Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Image(image: AssetImage('images/dice1.png'),
+             ),
+           ),
+         ) 
+        ],
+      ),
+    );
+  }
+}
