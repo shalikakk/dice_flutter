10a7a7e181823ee69187b5d9c6729b8dfa866c3b
shalika <shalikalahiru@gmail.com>
2020-01-19 07:52:05 +0530

    add setState method

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
 
+class _DicePageState extends State<DicePage> {
+  List<int> diceArry = [1, 2, 3, 4, 5, 6];
+  int randomValue = 1;
+  int randomValue2 = 1;
+  final _random = new Random();
+  @override
   Widget build(BuildContext context) {
-    var leftRandom = 3;
-    var rightRandom = 5;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-                child: Image(
-                image: AssetImage('images/dice$leftRandom.png'),
+              child: Image(
+                image: AssetImage('images/dice$randomValue.png'),
               ),
-              onPressed: (){
-
+              onPressed: () {
+                setState(() {
+                  
+                  randomValue = diceArry[_random.nextInt(diceArry.length)];
+                });
               },
             ),
           ),
           Expanded(
             child: FlatButton(
               child: Image(
-                image: AssetImage('images/dice$rightRandom.png'),
+                image: AssetImage('images/dice$randomValue2.png'),
               ),
-              onPressed: (){
-
+              onPressed: () {
+                setState(() {
+                  
+                  randomValue2 = diceArry[_random.nextInt(diceArry.length)];
+                });
               },
             ),
           ),
